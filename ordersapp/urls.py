cefd552cb69130e3d80147c3f319d261aff442af
Efremov Denis <ruzs.efremov@mail.ru>
2021-11-24 10:22:49 +0300

    CRUD for order

--- ordersapp/urls.py
+++ ordersapp/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('orders/read/', ordersapp.OrderList.as_view(), name='orders_list'),
     path('forming/complete/<int:pk>/', ordersapp.OrderList.as_view(), name='order_forming_complete'),
     path('create/', ordersapp.OrderItemsCreate.as_view(), name='order_create'),
-#   path('read/<int:pk>/', ordersapp.OrderRead.as_view(), name='order_read'),
-#   path('update/<int:pk>/', ordersapp.OrderItemsUpdate.as_view(), name='order_update'),
-#   path('delete/<int:pk>/', ordersapp.OrderDelete.as_view(), name='order_delete'),
+    path('read/<int:pk>/', ordersapp.OrderRead.as_view(), name='order_read'),
+    path('update/<int:pk>/', ordersapp.OrderItemsUpdate.as_view(), name='order_update'),
+    path('delete/<int:pk>/', ordersapp.OrderDelete.as_view(), name='order_delete'),
 ]
